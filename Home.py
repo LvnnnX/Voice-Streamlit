f2cbbdd6082eb82a479ed79e1f3f0e55e793f025
LvnnnX <rickyhadiman@gmail.com>
2025-03-14 14:50:01 +0800

    Adding requirements

--- Home.py
+++ Home.py
@@ -4,6 +4,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def clear_background():
+    st.markdown(
+    """
+<style>
+[data-testid^="stAppViewContainer"]{
+    background-color=black;
+
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+[data-testid^="stFormSubmitButton"] > button:first-child {
+    background-color: transparent;
+    text-align: center;
+    margin: 10;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+[data-testid^="stFormSubmitButton"]:hover > button:first-child {
+    border-color: green;
+}
+
+[class^="st-b"]  {
+    color: white;
+}
+[data-testid^="stMarkdownContainer"]{
+    background-color: transparent;
+    size: 20px;
+    color: white;
+    weight: bold;
+}
+
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+
+[class^="main css-k1vhr4 egzxvld3"]{
+    background-color:#0e1117;
+}
+
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+clear_background()
+
 st.header('Voice Recognition Machine Learning')
 st.write('Voice Recognition is meant to identify people by the unique characteristics of their voices.')
 
