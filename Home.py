15a494da781afca906179a46457a70b60561acf5
LvnnnX <rickyhadiman@gmail.com>
2025-03-14 14:59:49 +0800

    turn back to container

--- Home.py
+++ Home.py
@@ -58,12 +58,12 @@ clear_background()
 st.header('Voice Recognition Machine Learning')
 st.write('Voice Recognition is meant to identify people by the unique characteristics of their voices.')
 
-st.image('https://www.altexsoft.com/static/blog-post/2023/11/34ecda72-b5aa-46aa-af21-8a7a48ed3921.jpg', use_column_width=True, caption='Source: Altexsoft')
+st.image('https://www.altexsoft.com/static/blog-post/2023/11/34ecda72-b5aa-46aa-af21-8a7a48ed3921.jpg', use_container_width=True, caption='Source: Altexsoft')
 
 st.header('Data Collection')
 st.write('Data collection is the process of gathering and measuring information on variables of interest, in an established systematic fashion that enables one to answer stated research questions, test hypotheses, and evaluate outcomes.')
 st.write('The data that we will be using obtained manually from our own voice recordings.')
-st.image('img/dataset.png', use_column_width=False, caption='Source: Author')
+st.image('img/dataset.png', use_container_width=False, caption='Source: Author')
 
 st.header('Data Preparation')
 st.subheader('Data Labeling')
@@ -72,7 +72,7 @@ st.write('Data labeling or annotation invovles marking raw data with the correct
 st.subheader('Feature Extraction - Time-frequency domain features')
 st.write('This domain integrates both time and frequency elements, utilizing different types of spectograms to visually represent sound. A spectogram can be derived from a waveform by applying the short-time Fourier transform. Among the most widely used features in the time-frequency domain are mel-frequency cepstral coefficients (MFCCs). These features operate within the range of human hearing and are thus based on the mel scale and mel spectogram previously mentioned.')
 
-st.image('https://www.altexsoft.com/media/2022/05/word-image.jpeg', use_column_width=True, caption='Source: Altexsoft')
+st.image('https://www.altexsoft.com/media/2022/05/word-image.jpeg', use_container_width=True, caption='Source: Altexsoft')
 
 st.header('Model Building')
 st.subheader('Convolutional Neural Network (CNN)')
